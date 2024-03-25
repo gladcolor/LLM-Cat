76023a6a6e928a3696e2036fca9dfd0e97b9a658
Huan Ning <gladcolor@gmail.com>
2024-03-25 18:37:15 -0400

    added files.

--- LLM_Cat_Constants.py
+++ LLM_Cat_Constants.py
@@ -10,22 +10,22 @@ OpenAI_key = config.get('API_Key', 'OpenAI_key')
 # carefully change these prompt parts!   
 
 #--------------- constants for graph generation  ---------------
-graph_role = r'''A professional Geo-information scientist and programmer good at Python. You have worked on Geographic information science more than 20 years, and know every detail and pitfall when processing spatial data and coding. You know well how to set up workflows for spatial analysis tasks. You have significant experence on graph theory, application, and implementation. You are also experienced on generating map using Matplotlib and GeoPandas.
+graph_role = r'''A professional cartographer and programmer good at Python. You have worked on cartography more than 20 years, and know every detail and pitfall when visulizing spatial data and coding. You know well how to set up workflows for cartography tasks. You have significant experence on visualizing spatial data and graph theory. You are also experienced on generating map using Matplotlib, GeoPandas and other interative Python packages, such as Plotly.
 '''
 
-graph_task_prefix = r'Generate a graph (data structure) only, whose nodes are (1) a series of consecutive steps and (2) data to solve this question: '
+graph_task_prefix = r'Generate a graph (data structure) only, whose nodes are a series of consecutive steps to make a map, including visualize the data, add map element such as title, legend, scalebar, label, and annotaion: '
 
 graph_reply_exmaple = r"""
 ```python
 import networkx as nx
 G = nx.DiGraph()
 # Add nodes and edges for the graph
-# 1 Load hazardous waste site shapefile
-G.add_node("haz_waste_shp_url", node_type="data", path="https://github.com/gladcolor/LLM-Geo/raw/master/overlay_analysis/Hazardous_Waste_Sites.zip", description="Hazardous waste facility shapefile URL")
-G.add_node("load_haz_waste_shp", node_type="operation", description="Load hazardous waste facility shapefile")
-G.add_edge("haz_waste_shp_url", "load_haz_waste_shp")
-G.add_node("haz_waste_gdf", node_type="data", description="Hazardous waste facility GeoDataFrame")
-G.add_edge("load_haz_waste_shp", "haz_waste_gdf")
+# 1 Load boundary shapefile
+G.add_node("boundary_shp_url", node_type="data", path="https://xxx/boundary.zip", description="Boundary shapefile URL")
+G.add_node("load_boudary_shp", node_type="operation", description="Load boundary shapefile")
+G.add_edge("boundary_shp_url", "load_boudary_shp")
+G.add_node("boundary_gdf", node_type="data", description="Boundary GeoDataFrame")
+G.add_edge("load_boudary_shp", "boundary_gdf")
 ...
 ```
 """
@@ -37,9 +37,9 @@ graph_requirement = [
                         'The input of each operation is the output of the previous operations, except the those need to load data from a path or need to collect data.',
                         'You need to carefully name the output data node, making they human readable but not to long.',
                         'The data and operation form a graph.',
-                        'The first operations are data loading or collection, and the output of the last operation is the final answer to the task.'
+                        'The first operations are data loading or collection, and the output of the last operation is to generat map(s).',
                         'Operation nodes need to connect via output data nodes, DO NOT connect the operation node directly.',
-                        'The node attributes include: 1) node_type (data or operation), 2) data_path (data node only, set to "" if not given ), and description. E.g., {‘name’: “County boundary”, “data_type”: “data”, “data_path”: “D:\Test\county.shp”,  “description”: “County boundary for the study area”}.',
+                        'The node attributes include: 1) node_type (data or operation), 2) data_path (data node only, set to "" if not given ), and description. E.g., {"name": "County boundary", "data_type": "data", "data_path": "D:\Test\county.shp",  "description": "County boundary for the study area"}.',
                         'The connection between a node and an operation node is an edge.', 
                         'Add all nodes and edges, including node attributes to a NetworkX instance, DO NOT change the attribute names.',
                         'DO NOT generate code to implement the steps.',
@@ -47,21 +47,20 @@ graph_requirement = [
                         'Put your reply into a Python code block, NO explanation or conversation outside the code block(enclosed by ```python and ```).',
                         'Note that GraphML writer does not support class dict or list as data values.',
                         'You need spatial data (e.g., vector or raster) to make a map.',
+                        'A map usualy contains several map elements, such as title, north arrow scalebar, legend, label, and annotation.',
+                        'Create an operation node for each map element, you can determine which element is needed according to user input or based on your reasoning.',
+                        'Add a map element via a individual operation node. The nodes of adding map elements should be successive.',
                         'Do not put the GraphML writing process as a step in the graph.',
                         'Keep the graph concise, DO NOT use too many operation nodes.',
-                        # 'Keep the graph concise, DO NOT over-split task into too many small steps, especially for simple problems. For example, data loading and data transformation/preprocessing should be in one operation node.',
-
+  
                          ]
 
-# other requirements prone to errors, not used for now
-"""
-'DO NOT over-split task into too many small steps, especially for simple problems. For example, data loading and data transformation/preprocessing should be in one step.',
-"""
+ 
 
 
 
 #--------------- constants for operation generation  ---------------
-operation_role = r'''A professional Geo-information scientist and programmer good at Python. You have worked on Geographic information science more than 20 years, and know every detail and pitfall when processing spatial data and coding. You know well how to design and implement a function that meet the interface between other functions. Yor program is always robust, considering the various data circumstances, such as colum data types, avoiding mistakes when joining tables, and remove NAN cells before further processing. You have an good feeling of overview, meaning the functions in your program is coherent, and they connect to each other well, such as function names, parameters types, and the calling orders. You are also super experienced on generating maps using GeoPandas and Matplotlib.
+operation_role = r'''A professional cartographer and programmer good at Python. You have worked on cartographer more than 20 years, and know every detail and pitfall when processing and visualizing spatial data and using code to genearte the map. You know well how to design and implement a function that meet the interface between other functions. Yor program is always robust, considering the various data circumstances, such as colum data types, avoiding mistakes when joining tables, and remove NAN cells before further processing. You have an good feeling of overview, meaning the functions in your program is coherent, and they connect to each other well, such as function names, parameters types, and the calling orders. You are also super experienced on generating maps using GeoPandas, Matplotlib and Plotly.
 '''
 
 operation_task_prefix = r'You need to generate a Python function to do: '
@@ -80,60 +79,29 @@ operation_requirement = [
                         'DO NOT change the given variable names and paths.',
                         'Put your reply into a Python code block(enclosed by ```python and ```), NO explanation or conversation outside the code block.',
                         'If using GeoPandas to load a zipped ESRI shapefile from a URL, the correct method is "gpd.read_file(URL)". DO NOT download and unzip the file.',
-                        # "Generate descriptions for input and output arguments.",
                         "You need to receive the data from the functions, DO NOT load in the function if other functions have loaded the data and returned it in advance.",
-                        # "Note module 'pandas' has no attribute or method of 'StringIO'",
                         "Use the latest Python modules and methods.",
                         "When doing spatial analysis, convert the involved spatial layers into the same map projection, if they are not in the same projection.",
-                        # "DO NOT reproject or set spatial data(e.g., GeoPandas Dataframe) if only one layer involved.",
-                        "Map projection conversion is only conducted for spatial data layers such as GeoDataFrame. DataFrame loaded from a CSV file does not have map projection information.",
-                        "If join DataFrame and GeoDataFrame, using common columns, DO NOT convert DataFrame to GeoDataFrame.",
-                        # "When joining tables, convert the involved columns to string type without leading zeros. ",
-                        # "When doing spatial joins, remove the duplicates in the results. Or please think about whether it needs to be removed.",
-                        # "If using colorbar for GeoPandas or Matplotlib visulization, set the colorbar's height or length as the same as the plot for better layout.",
-                        "Graphs or maps need to show the unit, legend, or colorbar.",
                         "Keep the needed table columns for the further steps.",
                         "Remember the variable, column, and file names used in ancestor functions when using them, such as joining tables or calculating.",                        
-                        # "When crawl the webpage context to ChatGPT, using Beautifulsoup to crawl the text only, not all the HTML file.",
-                        "If using GeoPandas for spatial analysis, when doing overlay analysis, carefully think about use Geopandas.GeoSeries.intersects() or geopandas.sjoin(). ",
-                        "Geopandas.GeoSeries.intersects(other, align=True) returns a Series of dtype('bool') with value True for each aligned geometry that intersects other. other:GeoSeries or geometric object. ",
-                        "If using GeoPandas for spatial joining, the arguements are: geopandas.sjoin(left_df, right_df, how='inner', predicate='intersects', lsuffix='left', rsuffix='right', **kwargs), how: the type of join, default ‘inner’, means use intersection of keys from both dfs while retain only left_df geometry column. If 'how' is 'left': use keys from left_df; retain only left_df geometry column, and similarly when 'how' is 'right'. ",
-                        "Note geopandas.sjoin() returns all joined pairs, i.e., the return could be one-to-many. E.g., the intersection result of a polygon with two points inside it contains two rows; in each row, the polygon attributes are the same. If you need of extract the polygons intersecting with the points, please remember to remove the duplicated rows in the results.",
-
                         "DO NOT use 'if __name__ == '__main__:' statement because this program needs to be executed by exec().",
                         "Use the Python built-in functions or attribute. If you do not remember, DO NOT make up fake ones, just use alternative methods.",
                         "Pandas library has no attribute or method 'StringIO', so 'pd.compat.StringIO' is wrong, you need to use 'io.StringIO' instead.",
-                        "Before using Pandas or GeoPandas columns for further processing (e.g. join or calculation), drop recoreds with NaN cells in those columns, e.g., df.dropna(subset=['XX', 'YY']).",
-                        "When read FIPS or GEOID columns from CSV files, read those columns as str or int, never as float.",
-                        "FIPS or GEOID columns may be str type with leading zeros (digits: state: 2, county: 5, tract: 11, block group: 12), or integer type without leading zeros. Thus, when joining they, you can convert the integer colum to str type with leading zeros to ensure the success.",
                         "If you need to make a map and the map size is not given, set the map size to 15*10 inches.",
                         ]
-# other requirements prone to errors, not used for now
-"""
-If joining FIPS or GEOID, need to fill the leading zeros (digits: state: 2, county: 5, tract: 11, block group: 12.
-"Create a copy or use .loc to avoid SettingWithCopyWarning when using pandas DataFrames."
-"When creating maps or graphs, make them looks beautiful and professional. Carefuly select color, and show the layout, aspect, size, legend, scale bar, colorbar, background, annotation, axis ticks, title, font size, and label appropriately, but not overloaded."
- "Drop rows with NaN cells, i.e., df.dropna(),  before using Pandas or GeoPandas columns for processing (e.g. join or calculation).",
- # "GEOID in US Census data and FIPS in Census boundaries are integer with leading zeros. If use pandas.read_csv() to GEOID or FIPS (or 'fips') columns from read CSV files, set the dtype as 'str'.",
- # "Show a progressbar (e.g., tqdm in Python) if loop more than 200 times, also add exception handling for loops to make sure the loop can run.",
-"""
 
 
 #--------------- constants for assembly prompt generation  ---------------
-assembly_role =  r'''A professional Geo-information scientist and programmer good at Python. You have worked on Geographic information science more than 20 years, and know every detail and pitfall when processing spatial data and coding. Your are very good at assembling functions and small programs together. You know how to make programs robust.
+assembly_role =  r'''A professional cartographer and programmer good at Python. You have worked on cartography more than 20 years, and know every detail and pitfall when visualizing spatial data and coding. Your are very good at assembling functions and small programs together. You know how to make programs robust.
 '''
 
 assembly_requirement = ['You can think step by step. ',
-                    f"Each function is one step to solve the question. ",
-                    f"The output of the final function is the question to the question.",
-                    f"Put your reply in a code block(enclosed by ```python and ```), NO explanation or conversation outside the code block.",              
-                    f"Save final maps, if any. If use matplotlib, the function is: matplotlib.pyplot.savefig(*args, **kwargs).",
-                    f"The program is executable, put it in a function named 'assembely_solution()' then run it, but DO NOT use 'if __name__ == '__main__:' statement because this program needs to be executed by exec().",
+                    "Each function is one step to solve the question. ",
+                    "The output of the final function is the question to the question.",
+                    "Put your reply in a code block(enclosed by ```python and ```), NO explanation or conversation outside the code block.",              
+                    "Save final maps, if any. If use matplotlib, the function is: matplotlib.pyplot.savefig(*args, **kwargs).",
+                    "The program is executable, put it in a function named 'assembely_solution()' then run it, but DO NOT use 'if __name__ == '__main__:' statement because this program needs to be executed by exec().",
                     "Use the built-in functions or attribute, if you do not remember, DO NOT make up fake ones, just use alternative methods.",
-                    # "Drop rows with NaN cells, i.e., df.dropna(),  before using Pandas or GeoPandas columns for processing (e.g. join or calculation).",
-                    "If using GeoPandas for spatial analysis, when doing overlay analysis, carefully think about use Geopandas.GeoSeries.intersects() or geopandas.sjoin(). ",
-                    "Geopandas.GeoSeries.intersects(other, align=True) returns a Series of dtype('bool') with value True for each aligned geometry that intersects other. other:GeoSeries or geometric object. ",
-                    "Note geopandas.sjoin() returns all joined pairs, i.e., the return could be one-to-many. E.g., the intersection result of a polygon with two points inside it contains two rows; in each row, the polygon attribute is the same. If you need of extract the polygons intersecting with the points, please remember to remove the duplicated rows in the results.",
                     ]
 
 #--------------- constants for direct request prompt generation  ---------------
